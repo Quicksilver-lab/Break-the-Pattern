7398c79ab0a478de9d1934c31e8c537c82c38de5
Quicksilver-lab <145604505+Quicksilver-lab@users.noreply.github.com>
2024-06-11 18:46:18 +0500

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,32 @@
+const config = {
+  GAME: {
+    DIFFICULTY: {
+      EASY: 4,
+      MEDIUM: 5,
+      HARD: 6
+    },
+    TYPE: {
+      PRACTICE: 1,
+      CHALLENGE: 2,
+      COUNTDOWN: 3
+    },
+    ACTIONS: {
+      CONTINUE: 1,
+      NEW_GAME: 2,
+      BACK_HOME: 3
+    }
+  },
+  URL: 'https:/code-projects.org/',
+  COLORS: {
+    BRIGHT:  '#ffffff',
+    DARK:    '#14171b',
+    SUCCESS: '#116699',
+    ERROR:   '#ff0000'
+  },
+  PATTERN: {
+    HEX_COLOR_START: '66',
+    HEX_COLOR_END:   'FF'
+  }
+}
+
+export default config
